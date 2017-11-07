ba40c2969e2f60e5aaec9c13711804a234e34886
Ing.Norante <eppemela@users.noreply.github.com>
2017-11-07 18:48:04 +0100

    Update Andrea Address

--- contracts/GongoloSplitter.sol
+++ contracts/GongoloSplitter.sol
@@ -6,7 +6,7 @@ contract GongoloSplitter {
   address matteo = 0xcB5C7016F0dC9A368B403216B6998AfcCeFbe3d5;
   address giuseppe = 0x7B3B5C3175D8BeEbae37419C174295975E0F1A29;
   address silvio = 0x15E6316E136e7DA5432D81af81E259BDa33F6F6f;
-  address andrea = 0xadfe237e909Bba2cAcae989b1BdC275E110C06fb;
+  address andrea = 0xDfC9dE0A9b301Cf28192705121E8958af23d5819;
 
   event EthReceived(address sender, uint amount);
   event EthSent(address beneficiary, uint amount);
